cadb8c9529d1d30d38c4cdd2b97b6fa53e5b2575
marcvanduivenvoorde <marcvanduivenvoorde@gmail.com>
2016-02-23 21:40:41 +0100

    - first try in the mixer reset.

--- reset_mixer.py
+++ reset_mixer.py
@@ -1,13 +1,18 @@
 from reaper_python import *
+import PerimeterAudio.tracks
 
 # RPR_Main_SaveProject(0, True)
-path = RPR_GetProjectPathEx(0)
+# path = RPR_GetProjectPathEx(0)
 
-print path
+# print path
 
-for track_number in range(0, RPR_CountTracks(0) + 1):
-    track = RPR_GetTrack(0, track_number)
+# for track_number in range(0, RPR_CountTracks(0) + 1):
 
+    # track = RPR_GetTrack(0, track_number)
+
+
+
+for track in PerimeterAudio.tracks.get_tracks():
     RPR_SetMediaTrackInfo_Value(track, "D_VOL", 1.0)
     RPR_SetMediaTrackInfo_Value(track, "D_PAN", 0)
     RPR_SetMediaTrackInfo_Value(track, "I_SELECTED", 1)

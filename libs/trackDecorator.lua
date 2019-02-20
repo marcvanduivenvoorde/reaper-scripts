24a004cc5f87db6cbe72fa8bae250afcdf6d7234
Marc van Duivenvoorde <marc.vanduivenvoorde@synetic.nl>
2019-02-20 15:19:13 +0100

    - add decorator library - experiment more

---
+++ libs/trackDecorator.lua
@@ -0,0 +1,68 @@
+local trackDecorator = {}
+
+trackDecorator.colors = {}
+trackDecorator.colors.drums = 26316
+trackDecorator.colors.guitars = 8849466
+trackDecorator.colors.bass = 6293644
+trackDecorator.colors.vocals = 12619858
+
+function trackDecorator.decorateSnare(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'snare __dr__')
+end
+
+function trackDecorator.decorateKick(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'kick __dr__')
+end
+
+function trackDecorator.decorateTom(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'tom __dr__')
+end
+
+function trackDecorator.decorateOverhead(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'overhead __dr__')
+end
+
+function trackDecorator.decorateRoom(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'room __dr__')
+end
+
+function trackDecorator.decorateGuitar(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'guitar __gtr__')
+end
+
+function trackDecorator.decorateGuitar(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'guitar __gtr__')
+end
+
+function trackDecorator.decorateGuitar(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'guitar solo __gtr__')
+end
+
+function trackDecorator.decorateBass(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'bass __bass__')
+end
+
+function trackDecorator.decorateVocal(currentTrack)
+    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.vocals, 'vocal __vox__')
+end
+
+function createPostFaderSend(currentTrack, receivingTrack)
+    sendIndex = reaper.CreateTrackSend(currentTrack, receivingTrack)
+    reaper.SetTrackSendInfo_Value(currentTrack, 0, sendIndex, "I_SENDMODE", 0)
+end
+
+-- the eventual decorator
+function trackDecorator.decorate(currentTrack, color, name)
+    reaper.SetTrackColor(currentTrack, color)
+    reaper.GetSetMediaTrackInfo_String(currentTrack, "P_NAME", name, true)
+end
--- tmp-test-file.lua
+++ tmp-test-file.lua
@@ -1,6 +1,7 @@
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 
 trackFinder = require("libs.TrackFinder")
+trackDecorator = require('libs.trackDecorator')
 
 -- setup the track color for the new track
 -- add a postfader send to the snare bus track
@@ -8,18 +9,12 @@ function main()
     receivingTrack = trackFinder.find('snare-bus __dr__')
     currentTrack = reaper.GetSelectedTrack(0, 0)
 
-    reaper.SetTrackColor(currentTrack, 26316)
-
-    sendIndex = reaper.CreateTrackSend(currentTrack, receivingTrack)
-
-    reaper.GetSetMediaTrackInfo_String(currentTrack, "P_NAME", "snare __dr__", true)
-    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", false, true)
-    reaper.SetTrackSendInfo_Value(currentTrack, 0, sendIndex, "I_SENDMODE", 0)
+    trackDecorator.createPostFaderSend(currentTrack, receivingTrack)
+    trackDecorator.decorateSnare(currentTrack)
 
     if (sendIndex == 0) then
         reaper.ShowMessageBox('Could not create send.')
     end
-
 end
 
 reaper.defer(main)

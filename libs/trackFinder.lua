47ea7d4d52039902c9885a5a08f122092962fb2a
Marc van Duivenvoorde <marc.vanduivenvoorde@synetic.nl>
2019-02-20 14:50:28 +0100

    - library test

--- libs/trackFinder.lua
+++ libs/trackFinder.lua
@@ -1,4 +1,4 @@
-local trackFinder
+local trackFinder = {}
 -- find a track by it's name, show error and return false when not found
 function trackFinder.find(trackName)
     for trackId = 0, reaper.CountTracks(0) - 1 do

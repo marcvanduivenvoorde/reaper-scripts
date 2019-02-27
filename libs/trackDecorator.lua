229239804a8a9cecbce70cae9d4149e4ca47a3b6
Marc van Duivenvoorde <marc.vanduivenvoorde@synetic.nl>
2019-02-27 15:03:55 +0100

    - update config - fix vca slave settings - update decoration - remove all sends before adding new send

--- libs/trackDecorator.lua
+++ libs/trackDecorator.lua
@@ -6,61 +6,86 @@ trackDecorator.colors.drums = 26316
 trackDecorator.colors.guitars = 8849466
 trackDecorator.colors.bass = 6293644
 trackDecorator.colors.vocals = 12619858
-trackDecorator.vca.drums = "Group 1"
+trackDecorator.vca.drums = 1
+trackDecorator.vca.guitars = 2
+trackDecorator.vca.bass = 3
+trackDecorator.vca.vocals = 4
+
 
 function trackDecorator.decorateSnare(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
     trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
+
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'snare __dr__')
 end
 
 function trackDecorator.decorateKick(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'kick __dr__')
 end
 
 function trackDecorator.decorateTom(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
+
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'tom __dr__')
 end
 
 function trackDecorator.decorateOverhead(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
+
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'overhead __dr__')
 end
 
 function trackDecorator.decorateRoom(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
+
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'room __dr__')
 end
 
 function trackDecorator.decorateGuitar(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'guitar __gtr__')
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.guitars)
+
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitars, 'guitar __gtr__')
 end
 
-function trackDecorator.decorateGuitar(currentTrack)
+function trackDecorator.decorateGuitarSolo(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'guitar __gtr__')
-end
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.guitars)
 
-function trackDecorator.decorateGuitar(currentTrack)
-    reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'guitar solo __gtr__')
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitars, 'guitar __gtr__')
 end
 
 function trackDecorator.decorateBass(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitar, 'bass __bass__')
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.bass)
+
+    return trackDecorator.decorate(currentTrack, trackDecorator.colors.bass, 'bass __bass__')
 end
 
 function trackDecorator.decorateVocal(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
+    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.vocals)
+
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.vocals, 'vocal __vox__')
 end
 
 function trackDecorator.addToVcaGroup(currentTrack, group)
-    reaper.GetSetTrackGroupMembership(currentTrack, "VOLUME_VCA_SLAVE", 0, 1)
+    setValue = 1 << (group -1)
+
+    reaper.GetSetTrackGroupMembership(currentTrack, "VOLUME_VCA_SLAVE", setValue, setValue)
+end
+
+function trackDecorator.clearAllSends(currentTrack)
+    numberOfSends = reaper.GetTrackNumSends(currentTrack, 0)
+
+    for index = 0, numberOfSends do
+        reaper.RemoveTrackSend(currentTrack, 0, index)
+    end
 end
 
 function trackDecorator.createPostFaderSend(currentTrack, receivingTrack)
--- setup-as-bass-track.lua
+++ setup-as-bass-track.lua
@@ -1,6 +1,6 @@
 package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
 
-trackFinder = require("libs.TrackFinder")
+trackFinder = require("libs.trackFinder")
 trackDecorator = require('libs.trackDecorator')
 
 -- setup the track color for the new track
@@ -9,6 +9,7 @@ function main()
     receivingTrack = trackFinder.find('bass-bus __bass__')
     currentTrack = reaper.GetSelectedTrack(0, 0)
 
+    trackDecorator.clearAllSends(currentTrack)
     trackDecorator.createPostFaderSend(currentTrack, receivingTrack)
     trackDecorator.decorateBass(currentTrack)
 end

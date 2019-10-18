68eba20f287225e991a22a8af2b40e841a53aecf
Marc van Duivenvoorde <marc.van.duivenvoorde@create.nl>
2019-10-18 15:20:58 +0200

    - freeze to mono and recolor selected tracks

---
+++ freeze-to-mono-and-recolor.lua
@@ -0,0 +1,16 @@
+
+function main()
+    local color = 15790839
+    reaper.Main_OnCommandEx(40901, 0, 0);
+    trackCount = reaper.CountTracks(0) - 1
+
+    for trackId = 0, trackCount do
+        track = reaper.GetTrack(0, trackId)
+
+        if reaper.IsTrackSelected(track) then
+            reaper.SetTrackColor(track, color)
+        end
+    end
+end
+
+reaper.defer(main)

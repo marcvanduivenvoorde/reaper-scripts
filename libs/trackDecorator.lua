ad4e12a2d2261ee2b61b5bdf96a52d1f91f31e78
Marc van Duivenvoorde <marc.van.duivenvoorde@create.nl>
2020-02-28 09:03:50 +0100

    - new branch for the new template - update some code

--- libs/trackDecorator.lua
+++ libs/trackDecorator.lua
@@ -2,12 +2,12 @@ local trackDecorator = {}
 
 trackDecorator.colors = {}
 trackDecorator.vca = {}
-trackDecorator.colors.drums = 26316
-trackDecorator.colors.guitars = 8849466
-trackDecorator.colors.guitarsolo = 13408767
-trackDecorator.colors.bass = 6293644
-trackDecorator.colors.vocals = 12619858
-trackDecorator.colors.synth = 16724991
+trackDecorator.colors.drums = 8323073
+trackDecorator.colors.guitars = 4227076
+trackDecorator.colors.guitarsolo = 4227076
+trackDecorator.colors.bass = 540800
+trackDecorator.colors.vocals = 16613382
+trackDecorator.colors.synth = 8388737
 trackDecorator.vca.drums = 1
 trackDecorator.vca.guitars = 2
 trackDecorator.vca.guitarsolo = 3
@@ -36,124 +36,68 @@ end
 
 function trackDecorator.decorateSnare(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.snare)
-
-    trackDecorator.prepareParallelDrumTrack(currentTrack)
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'snare-paracomp __dr__ __snare__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'snare-reverb __dr__ __snare__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'drum-shell-snap __dr__');
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'snare', '__dr__ __snare__')
 end
 
-function trackDecorator.prepareParallelDrumTrack(currentTrack)
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'drum-paracomp __dr__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'drum-reverb __dr__')
-end
-
-function trackDecorator.prepareParallelVocalTrack(currentTrack)
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'vocal-paracomp __vox__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'vocal-reverb __vox__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'vocal-delay __vox__')
-end
-
 function trackDecorator.decorateKick(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.prepareParallelDrumTrack(currentTrack)
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'drum-shell-snap __dr__')
 
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.kick)
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'kick', '__dr__ __kick__')
 end
 
 function trackDecorator.decorateTom(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.prepareParallelDrumTrack(currentTrack);
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'drum-shell-snap __dr__');
-
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'tom', '__dr__ __tom__')
 end
 
 function trackDecorator.decorateOverhead(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.prepareParallelDrumTrack(currentTrack);
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'overhead', '__dr__ __oh__')
 end
 
 function trackDecorator.decorateRoom(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.prepareParallelDrumTrack(currentTrack);
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.drums)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.drums, 'room', '__dr__ __room__')
 end
 
 function trackDecorator.decorateGuitar(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.guitars)
-
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'guitar-reverb __gtr__')
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitars, 'guitar', '__gtr__')
 end
 
 function trackDecorator.decorateGuitarSolo(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.guitarsolo)
-
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'guitar-solo-delay __gtr__ __gtrs__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'guitar-solo-reverb __gtr__ __gtrs__')
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.guitarsolo, 'guitar solo', '__gtr__ __gtrs__')
 end
 
 function trackDecorator.decorateSynth(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.synth)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.synth, 'synth', '__syn__')
 end
 
 function trackDecorator.decorateBass(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.bass)
-
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'bass-reverb __bass__')
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.bass, 'bass', '__bass__')
 end
 
 function trackDecorator.decorateVocal(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.prepareParallelVocalTrack(currentTrack)
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.vocals)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.vocals, 'vocal', '__vox__')
 end
 
 function trackDecorator.decorateBackupVocal(currentTrack)
     reaper.SetMediaTrackInfo_Value(currentTrack, "B_MAINSEND", 0)
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'backup-vocal-delay __vox__')
-    trackDecorator.sendParallelProcessTrack(currentTrack, 'backup-vocal-reverb __vox__')
-    trackDecorator.addToVcaGroup(currentTrack, trackDecorator.vca.vocals)
 
     return trackDecorator.decorate(currentTrack, trackDecorator.colors.vocals, 'vocal', '__vox__')
-end
-
-
-
-function trackDecorator.addToVcaGroup(currentTrack, group)
-    setValue = 1 << (group -1)
-
-    reaper.GetSetTrackGroupMembership(currentTrack, "VOLUME_VCA_SLAVE", setValue, setValue)
-    reaper.GetSetTrackGroupMembership(currentTrack, "SOLO_SLAVE", setValue, setValue)
-    reaper.GetSetTrackGroupMembership(currentTrack, "MUTE_SLAVE", setValue, setValue)
 end
 
 function trackDecorator.clearAllSends(currentTrack)

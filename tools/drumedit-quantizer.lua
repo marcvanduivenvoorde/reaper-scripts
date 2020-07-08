fed0761a10496535dc4e0053c3aec582ffddc83e
Marc van Duivenvoorde <marc.van.duivenvoorde@create.nl>
2020-07-08 12:13:14 +0200

    - fix bugs

--- tools/drumedit-quantizer.lua
+++ tools/drumedit-quantizer.lua
@@ -10,26 +10,33 @@ function main ()
             reaper.SetMediaItemLength(item, 0.025, true)
             reaper.SetMediaItemInfo_Value(item, 'D_FADEINLEN', 0)
             reaper.SetMediaItemInfo_Value(item, 'D_FADEOUTLEN', 0)
-            command = reaper.NamedCommandLookup('_FNG_QUANTIZE_TO_GRID')
-            reaper.UpdateItemInProject(item)
-            reaper.Main_OnCommandEx(command, 0, 0) -- normalize the selected items
         end
     end
+    command = reaper.NamedCommandLookup('_FNG_QUANTIZE_TO_GRID')
+    reaper.Main_OnCommandEx(command, 0, 0) -- normalize the selected items
 
     for itemId = 0, itemCount do
         item = reaper.GetMediaItem(0, itemId)
+        itemTrack = reaper.GetMediaItemInfo_Value(item, 'P_TRACK')
+        trackId = reaper.GetTrackGUID(itemTrack)
 
         if reaper.IsMediaItemSelected(item) then
             start = reaper.GetMediaItemInfo_Value(item, 'D_POSITION')
             nextItem = reaper.GetMediaItem(0, itemId + 1)
 
-            if nextItem then
-                nextStart = reaper.GetMediaItemInfo_Value(nextItem, 'D_POSITION')
+            if nextItem ~= nil then
+                nextItemTrack = reaper.GetMediaItemInfo_Value(nextItem, 'P_TRACK')
+                nextTrackId = reaper.GetTrackGUID(nextItemTrack)
 
-                if (nextStart >= start) then
-                    length = nextStart - start - 0.025
-                    reaper.SetMediaItemLength(item, length, true)
+                if trackId == nextTrackId then
+                    nextStart = reaper.GetMediaItemInfo_Value(nextItem, 'D_POSITION')
+
+                    if (nextStart >= start) then
+                        length = nextStart - start - 0.025
+                        reaper.SetMediaItemLength(item, length, true)
+                    end
                 end
+
             end
         end
     end

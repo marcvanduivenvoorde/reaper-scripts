942d0e5cd146b13b8f2e7a1fd0473e7eb793c48d
Marc van Duivenvoorde <marc.van.duivenvoorde@create.nl>
2020-07-08 10:44:11 +0200

    - set the default item volume lower so there is a bit of headroom left after normalizing

--- tools/dreamgate-track-selected-items.lua
+++ tools/dreamgate-track-selected-items.lua
@@ -12,10 +12,14 @@ function main ()
             reaper.SetMediaItemInfo_Value(item, 'D_FADEOUTLEN', 0.025)
             reaper.SetMediaItemInfo_Value(item, 'C_FADEINSHAPE', 6)
             reaper.SetMediaItemInfo_Value(item, 'C_FADEOUTSHAPE', 5)
+            reaper.SetMediaItemInfo_Value(item, 'D_VOL', 0.7)
+            reaper.UpdateItemInProject(item)
         end
     end
 
     reaper.Main_OnCommandEx(40108, 0, 0) -- normalize the selected items
+    reaper.UpdateArrange()
+
     reaper.Undo_EndBlock('Split dreamgate', -1)
 end
 
